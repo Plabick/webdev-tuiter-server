ea5103a108d7100ee2d095a21b36e561340fa75d
Plabick <peterlabick@gmail.com>
2023-04-04 15:49:14 -0400

    Use MongoDB Server
    Fix CORS
    
    Fix CORS
    
    Fix CORS
    
    Fix CORS
    
    Fix DB creds

--- app.js
+++ app.js
@@ -6,13 +6,29 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
-const app = express()
+import mongoose from "mongoose";
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+const CONNECTION_STRING =  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+
+mongoose.connect(CONNECTION_STRING);
+console.log("Connected to MongoDB")
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
+
 
 
